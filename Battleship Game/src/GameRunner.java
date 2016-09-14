3e22aa208d2ef4bfe93ae4bba342839d48354b2d
nicklocascio <nicoloc4545@gmail.com>
2016-09-14 14:59:58 -0600

    A little more progress

--- Battleship Game/src/GameRunner.java
+++ Battleship Game/src/GameRunner.java
@@ -4,7 +4,8 @@ public class GameRunner
 	{
 	static int row;
 	static int column;
-	// Try to change the amount of delay based on part of game (i.e. delay(1000);)
+	static String [][] board= new String [10][10];
+	// Try to change the amount of delay based on part of game (i.e. delay(1000))
 	public static void delay()
 		{
 		try
@@ -17,8 +18,7 @@ public class GameRunner
 			}
 		}
 	public static void main(String[] args)
-		{
-		String [][] battleshipBoard= new String [10][10];
+		{		
 		System.out.println("Welcome to battleship. Prepare for the game of a lifetime!");
 		// TEAM NAME
 //		Scanner team = new Scanner(System.in);
@@ -36,78 +36,91 @@ public class GameRunner
 		Scanner player = new Scanner(System.in);
 		System.out.println("");
 		System.out.println("Please enter the spaces for the carrier first");
-		String shipSpace = player.nextLine();
-		switch(shipSpace.substring(0, 1))
-			{
-			case "A":
-			case "a":
+//		String shipSpace = player.nextLine();
+//		for(int i = 0; i < 5; i++)
+//			{
+			String shipSpace = player.nextLine();
+			switch(shipSpace.substring(0, 1))
 				{
-				row = 0;
-				break;
+				case "A":
+				case "a":
+					{
+					row = 0;
+					break;
+					}
+				case "B":
+				case "b":
+					{
+					row = 1;
+					break;
+					}
+				case "C":
+				case "c":
+					{
+					row = 2;
+					break;
+					}
+				case "D":
+				case "d":
+					{
+					row = 3;
+					break;
+					}
+				case "E":
+				case "e":
+					{
+					row = 4;
+					break;
+					}
+				case "F":
+				case "f":
+					{
+					row = 5;
+					break;
+					}
+				case "G":
+				case "g":
+					{
+					row = 6;
+					break;
+					}
+				case "H":
+				case "h":
+					{
+					row = 7;
+					break;
+					}
+				case "I":
+				case "i":
+					{
+					row = 8;
+					break;
+					}
+				case "J":
+				case "j":
+					{
+					row = 9;
+					break;
+					}
 				}
-			case "B":
-			case "b":
+			column = Integer.parseInt(shipSpace.substring(1)) - 1;
+			for(int row = 0; row < board.length; row++)
 				{
-				row = 1;
-				break;
+				for(int col = 0; col < board.length; col++)
+					{
+					board[row][col] = "x";
+					}
 				}
-			case "C":
-			case "c":
-				{
-				row = 2;
-				break;
-				}
-			case "D":
-			case "d":
-				{
-				row = 3;
-				break;
-				}
-			case "E":
-			case "e":
-				{
-				row = 4;
-				break;
-				}
-			case "F":
-			case "f":
-				{
-				row = 5;
-				break;
-				}
-			case "G":
-			case "g":
-				{
-				row = 6;
-				break;
-				}
-			case "H":
-			case "h":
-				{
-				row = 7;
-				break;
-				}
-			case "I":
-			case "i":
-				{
-				row = 8;
-				break;
-				}
-			case "J":
-			case "j":
-				{
-				row = 9;
-				break;
-				}
-			}
-		column = Integer.parseInt(shipSpace.substring(1)) - 1;
-		for(int row = 0; row < battleshipBoard.length; row++)
-			{
-			for(int col = 0; col < battleshipBoard.length; col++)
-				{
-//				battleshipBoard[row][col] = row;
-				}
-			}
+//			}
+
+//		for(int row = 0; row < battleshipBoard.length; row++)
+//			{
+//			for(int col = 0; col < battleshipBoard.length; col++)
+//				{
+//				battleshipBoard[row][col] = "cat";
+//				}
+//			}
+//		System.out.println(board);
 		}
 	}
 
